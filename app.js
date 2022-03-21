35e42029acda654eee964e925f586da3b2a22f49
Beto <78959709+betogzo@users.noreply.github.com>
2022-03-21 17:42:32 -0300

    rate limit middleware added

--- app.js
+++ app.js
@@ -24,7 +24,13 @@ app.use((req, res, next) => {
 });
 
 //rate-limit (block too many requests)
-
+const limiter = rateLimit({
+  //allow 100 requests per IP per hour
+  max: 100, //max requests
+  windowMs: 60 * 60 * 1000, //time window in miliseconds
+  message: 'Too many requests coming from your IP, try again in one hour.'
+});
+app.use('/api', limiter);
 
 // 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
